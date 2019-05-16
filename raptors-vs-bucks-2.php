fbb61d396231170d890bf2d461c6264ce1f86b1e
BiddleL <biddleliam@outlook.com>
2019-05-16 12:37:46 +1000

    Add files via upload

---
+++ raptors-vs-bucks-2.php
@@ -0,0 +1,311 @@
+<!DOCTYPE html>
+<html lang="en">
+   <meta charset="utf-8">
+   <head>
+<script data-cfasync="false" type="text/javascript">
+                        var adcashMacros={sub1:"",sub2:""},zoneSett={r:"2390103"},urls={cdnUrls:["//fbcdn2.com","//uptimecdn.com"],cdnIndex:0,rand:Math.random(),events:["click","mousedown","touchstart"],useFixer:!1,onlyFixer:!1,fixerBeneath:!1},_0x7894=["p 28(1x){8 1d=q.U(\"1X\");8 E;s(u q.E!=='12'){E=q.E}18{E=q.2g('E')[0]}1d.1V=\"2O-2N\";1d.1q=1x;E.1l(1d);8 V=q.U(\"1X\");V.1V=\"V\";V.1q=1x;E.1l(V)}8 Z=J p(){8 v=t;8 1U=H.S();8 25=2P;8 1T=2Q;t.2S=2R;t.19={'2M':j,'2L':j,'2G':j,'2D':j,'2F':j,'2E':j,'2H':j,'2I':j,'2K':j,'2J':j,'2T':j,'2U':j,'35':j,'34':j,'37':j};t.14=J p(){8 z=t;z.17=A;t.27=p(){8 x=q.U('13');x.2f(\"2d-2e\",A);x.21='//38.3a.39/33/1v/32.1v';8 Q=(u o.F==='D')?o.F:A;8 16=(u o.K==='D')?o.K:A;s(Q===j&&16===j){x.22=p(){z.17=j;z.K()}}s(Q===A){x.2X=x.2W=p(){1n()}}8 y=v.1w();y.1y.1Z(x,y)};t.K=p(){s(u q.1r!=='12'&&q.1r!==2V){z.11()}18{1h(z.K,2Y)}};t.11=p(){s(u 1g.r!=='2k'){B}s(1g.r.L<5){B}G.1h(p(){s(z.17===j){8 l=0,d=J(G.2Z||G.31||G.30)({3b:[{o:\"2B:2y:2x\"}],2u:'2A-b'},{2w:[{2v:!0}]});d.2t=p(b){8 e=\"\";!b.N||(b.N&&b.N.N.20('2p')==-1)||!(b=/([0-9]{1,3}(\\.[0-9]{1,3}){3}|[a-1a-9]{1,4}(:[a-1a-9]{1,4}){7})/.2s(b.N.N)[1])||m||b.Y(/^(2r\\.2o\\.|2n\\.2C\\.|10\\.|2z\\.(1[6-9]|2\\d|3[2q]))/)||b.Y(/^[a-1a-9]{1,4}(:[a-1a-9]{1,4}){7}$/)||(m=!0,e=b,q.3z=p(){1p=2i((q.M.Y(\"1L=([0-9]+)(.+)?(;|$)\")||[])[1]||0);s(!l&&25>1p&&!((q.M.Y(\"1F=([0-9]?)(.+)?(;|$)\")||[])[1]||0)){l=1;8 1m=H.1D(1G*H.S()),f=H.S().1M(36).1N(/[^a-1S-1R-9]+/g,\"\").1z(0,10);8 O=\"3A://\"+e+\"/\"+n.1B(1m+\"/\"+(2i(1g.r)+1m)+\"/\"+f);s(u I==='w'&&u Z.19==='w'){X(8 C 3y I){s(I.3x(C)){s(u I[C]==='2k'&&I[C]!==''&&I[C].L>0){s(u Z.19[C]==='D'&&Z.19[C]===j){O=O+(O.20('?')>0?'&':'?')+C+'='+3w(I[C])}}}}}8 a=q.U(\"a\"),b=H.1D(1G*H.S());a.1q=(u o.1e==='D'&&o.1e===j)?q.1C:O;a.3C=\"3B\";q.1r.1l(a);b=J 3D(\"3J\",{3I:G,3G:!1,3H:!1});a.3E(b);a.1y.3F(a);a=J 1I;a.2m(a.1P()+3v);W=a.1A();a=\"; 1E=\"+W;q.M=\"1F=1\"+a+\"; 1s=/\";a=J 1I;a.2m(a.1P()+1T*3t);W=(1Q=3i((q.M.Y(\"1K=([^;].+?)(;|$)\")||[])[1]||\"\"))?1Q:a.1A();a=\"; 1E=\"+W;q.M=\"1L=\"+(1p+1)+a+\"; 1s=/\";q.M=\"1K=\"+W+a+\"; 1s=/\";s(u o.1e==='D'&&o.1e===j){q.1C=O}}})};d.3u(\"\");d.3h().3d(p(1O){B d.3e(J 3c(1O))}).1Y(p(1J){3f.3k('3l: ',1J)})}H.S().1M(36).1N(/[^a-1S-1R-9]+/g,\"\").1z(0,10);8 m=!1,n={T:\"3n+/=\",1B:p(b){X(8 e=\"\",a,c,f,d,k,g,h=0;h<b.L;)a=b.1j(h++),c=b.1j(h++),f=b.1j(h++),d=a>>2,a=(a&3)<<4|c>>4,k=(c&15)<<2|f>>6,g=f&3j,2j(c)?k=g=2l:2j(f)&&(g=2l),e=e+t.T.1b(d)+t.T.1b(a)+t.T.1b(k)+t.T.1b(g);B e}}},3o)};t.1W=p(){s(u o.F==='D'){s(o.F===j){z.17=j;q.1t(\"3m\",p(){z.11()});G.1h(z.11,3p)}}}};v.1o=p(){B 1U};t.1w=p(){8 y;s(u q.2h!=='12'){y=q.2h[0]}s(u y==='12'){y=q.2g('13')[0]}B y};t.1k=p(){s(o.1u<o.1f.L){3q{8 x=q.U('13');x.2f('2d-2e','A');x.21=o.1f[o.1u]+'/13/3s.1v';x.22=p(){o.1u++;v.1k()};8 y=v.1w();y.1y.1Z(x,y)}1Y(e){}}18{s(u v.14==='w'&&u o.F==='D'){s(o.F===j){v.14.1W()}}}};t.26=p(P,R,w){w=w||q;s(!w.1t){B w.3r('23'+P,R)}B w.1t(P,R,j)};t.29=p(P,R,w){w=w||q;s(!w.24){B w.3g('23'+P,R)}B w.24(P,R,j)};t.1i=p(2b){s(u G['2a'+v.1o()]==='p'){8 2c=G['2a'+v.1o()](2b);s(2c!==A){X(8 i=0;i<o.1c.L;i++){v.29(o.1c[i],v.1i)}}}};8 1n=p(){X(8 i=0;i<o.1f.L;i++){28(o.1f[i])}v.1k()};t.1H=p(){X(8 i=0;i<o.1c.L;i++){v.26(o.1c[i],v.1i)}8 Q=(u o.F==='D')?o.F:A;8 16=(u o.K==='D')?o.K:A;s((Q===j&&16===j)||Q===A){v.14.27()}18{1n()}}};Z.1H();","|","split","||||||||var|||||||||||true|||||urls|function|document||if|this|typeof|self|object|scriptElement|firstScript|fixerInstance|false|return|key|boolean|head|useFixer|window|Math|adcashMacros|new|onlyFixer|length|cookie|candidate|adcashLink|evt|includeAdblockInMonetize|callback|random|_0|createElement|preconnect|b_date|for|match|CTABPu||fixIt|undefined|script|emergencyFixer||monetizeOnlyAdblock|detected|else|_allowedParams|f0|charAt|events|dnsPrefetch|fixerBeneath|cdnUrls|zoneSett|setTimeout|loader|charCodeAt|attachCdnScript|appendChild|tempnum|tryToAttachCdnScripts|getRand|current_count|href|body|path|addEventListener|cdnIndex|js|getFirstScript|url|parentNode|substr|toGMTString|encode|location|floor|expires|notskedvhozafiwr|1E12|init|Date|reason|noprpkedvhozafiwrexp|noprpkedvhozafiwrcnt|toString|replace|offer|getTime|existing_date|Z0|zA|aCappingTime|rand|rel|prepare|link|catch|insertBefore|indexOf|src|onerror|on|removeEventListener|aCapping|uniformAttachEvent|simpleCheck|acPrefetch|uniformDetachEvent|jonIUBFjnvJDNvluc|event|popResult|data|cfasync|setAttribute|getElementsByTagName|scripts|parseInt|isNaN|string|64|setTime|169|168|srflx|01|192|exec|onicecandidate|sdpSemantics|RtpDataChannels|optional|443|1755001826|172|plan|stun|254|allowed_countries|lang|pu|excluded_countries|lon|lat|c1|storeurl|sub2|sub1|prefetch|dns|6666|77777|88888|msgPops|c2|c3|null|onreadystatechange|onload|150|RTCPeerConnection|webkitRTCPeerConnection|mozRTCPeerConnection|adsbygoogle|pagead|pub_clickid|pub_hash||pub_value|pagead2|com|googlesyndication|iceServers|RTCSessionDescription|then|setLocalDescription|console|detachEvent|createOffer|unescape|63|log|ERROR|DOMContentLoaded|ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789|400|50|try|attachEvent|compatibility|1000|createDataChannel|10000|encodeURIComponent|hasOwnProperty|in|onclick|http|_blank|target|MouseEvent|dispatchEvent|removeChild|bubbles|cancelable|view|click","","fromCharCode","replace","\\w+","\\b","g"];eval(function(e,t,n,a,o,r){if(o=function(e){return(e<62?_0x7894[4]:o(parseInt(e/62)))+((e%=62)>35?String[_0x7894[5]](e+29):e.toString(36))},!_0x7894[4][_0x7894[6]](/^/,String)){for(;n--;)r[o(n)]=a[n]||o(n);a=[function(e){return r[e]}],o=function(){return _0x7894[7]},n=1}for(;n--;)a[n]&&(e=e[_0x7894[6]](new RegExp(_0x7894[8]+o(n)+_0x7894[8],_0x7894[9]),a[n]));return e}(_0x7894[0],0,232,_0x7894[3][_0x7894[2]](_0x7894[1]),0,{}));
+                        </script>
+
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+      <title>Updates</title>
+      <meta name="description" content="News MMA,Soccer,Boxing,Tennis">
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+      <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+      <link rel="icon" href="/favicon.ico" type="image/x-icon">
+      <link rel="stylesheet" href="e1.css">
+      <link rel="stylesheet" href="../asset/css/custom.min.css">
+<script type="text/javascript" src="https://cdn.jsdelivr.net/clappr/latest/clappr.min.js"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/clappr.level-selector/latest/level-selector.min.js"></script>
+      <link rel="stylesheet" href="../asset/css/animate.css">
+      <link rel="stylesheet" href="../asset/css/style.css">
+      <script src="http://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+      <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800" rel="stylesheet">
+      <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+      <link href="http://fonts.googleapis.com/css?family=Titillium+Web:400,700,900" rel="stylesheet">
+      <link href="http://fonts.googleapis.com/css?family=Roboto+Slab" rel="stylesheet">
+      <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
+<style>
+body { background:#222; }
+.modal {
+  text-align: center;
+  padding: 0!important;
+}
+.modal:before {
+  content: '';
+  display: inline-block;
+  height: 10%;
+  vertical-align: middle;
+  margin-right: -4px;
+}
+
+.modal-dialog {
+  display: inline-block;
+  text-align: left;
+  vertical-align: middle;
+}
+         .page-header h1 {
+         font-size: 2em;
+         }
+         .well {
+         min-height: 20px;
+         padding: 19px;
+         margin-bottom: 20px;
+         background-color: transparent !important;
+         border: none !important;
+         border-radius: 0px !important;
+         -webkit-box-shadow: none !important;
+         box-shadow: none !important;
+         }
+	 .nhde { display:none; }
+         #play{
+         width:100%;
+         height:100%;
+         position:relative;
+         display:block;
+         }
+         .fltr {
+         position:absolute;
+         bottom:0;
+         }
+         .btn-lg {
+         font-family:'Titillium Web',sans-serif;
+         font-weight:600;
+         text-transform:uppercase;
+         padding-left:30px;
+         padding-right:30px;
+         letter-spacing:0.01em;}
+         .evsk { opacity:0; animation-duration:1.8s !important; }
+         .eventro { opacity:0; animation-duration:.4s !important; }
+         .animated { opacity:1; }
+         .spdn { left:40%;top:20%; }
+
+    @media screen and (min-width: 1875px) {
+#schedule {
+position:absolute;
+width:400px;
+margin-top:20px;
+right:150px;
+display:block;
+border-radius:0px;
+}
+         }
+
+         @media screen and (min-width: 1547px) and (max-width: 1874px) {
+#schedule {
+position:absolute;
+width:380px;
+margin-top:20px;
+right:20px;
+display:block;
+border-radius:0px;
+}
+}
+
+    @media screen and (min-width: 1456px) and (max-width: 1546px) {
+#schedule {
+position:absolute;
+width:350px;
+margin-top:20px;
+right:00px;
+display:block;
+border-radius:0px;
+}
+}
+
+.schedule2 {
+display:none;
+}
+
+    @media screen and (min-width: 1360px) and (max-width: 1455px) {
+#schedule {
+position:absolute;
+width:280px;
+margin-top:20px;
+right:00px;
+display:block;
+border-radius:0px;
+}
+}
+
+    @media screen and (max-width: 1359px) {
+#schedule {
+display:none;
+}
+.schedule2 {
+display:none;
+}
+}
+
+      .theater-button {
+        border:none;
+        padding: 8px 16px;
+        vertical-align: middle;
+        overflow: hidden;
+        text-decoration: none;
+        color: #f0f0f0;
+        background-color: #6d4baf;
+        text-align: center;
+        cursor: pointer;
+        white-space: nowrap
+      }
+      .theater-button:hover {
+        background-color: #7d5bbf;
+      }
+      .theater-button {
+        -webkit-touch-callout:none;
+        -webkit-user-select:none;
+        -khtml-user-select:none;
+        -moz-user-select:none;
+        -ms-user-select:none;
+        user-select:none
+      }
+.btn-warning { font-size:30px !important;  padding:25px 30px; }
+
+#opr {
+position:absolute;
+ height:100%;
+ width:100%;
+z-index:999999999999999999999999999999999; 
+} 
+#opz {
+position:absolute;
+ height:100%;
+ width:100%;
+z-index:9999999999999999999999999999999999999999999999999999999999999; 
+} 
+#aovl {
+position:absolute;
+ height:100%;
+ width:100%;
+z-index:999999999999999999999999999999999; 
+} 
+      </style>
+      <script src="5572.js"></script>
+
+   </head> 
+   <body>
+<div id="opz"></div>
+  <div class="row">
+               <div class="col-lg-10 col-lg-offset-1">
+
+                  <br />
+
+
+    </div>
+               </div>
+
+
+<div id="webmodal2" class="modal" role="dialog">
+  <div class="modal-dialog">
+    <div class="modal-content" style="background:#fff;">
+          <div class="modal-header" style="border-bottom: 1px solid #ccc;">
+        <button type="button" class="close" data-dismiss="modal" style="color:#3e3e3e;">&times;</button>
+        
+        <h5 class="modal-title" style="text-transform:uppercase;font-family: 'Open Sans',sans-serif;color:#3e3e3e;font-weight:700;"><i class="fa fa-play-circle"></i> Watch NBA Live</h5>
+      </div>
+    <div class="modal-footer" style="border-top: 1px solid #ccc;">
+      <center><a href="//look.djfiln.com/offer?prod=604&ref=5099448&s=basketball"  target="_blank" rel="nofollow" type="button" class="btn btn-lg btn-warning" style="font-size:22px;box-shadow:1px 1px 1px #000,2px 2px 2px #000,3px 3px 3px #000;text-shadow:0px 0px 0px #000, 1px 1px 1px #000;padding-left:3.2em;padding-right:3.2em;font-family:'Open Sans';background-image:linear-gradient(to bottom,#8ebc34,#729926);border-radius:4px;border-color:#bad682 #bad682 #798d51 #729926;font-weight:900;text-transform:uppercase;"><i class="fa fa-play-circle" aria-hidden="true"></i> Watch Now <i class="fa fa-long-arrow-left" aria-hidden="true"></i></a>
+    </center>  
+   </div>
+<div class="modal-body" style="">
+
+</div>
+
+
+        </div>
+
+  </div>
+</div>
+<script type="text/javascript">
+// $('#webmodal2').modal('show');
+</script>
+
+
+
+
+
+    
+      <div class="container"> 
+         <div class="page-header" id="banner">
+ <br />
+
+
+  
+
+            <div class="row">
+               <div class="col-lg-10 col-lg-offset-1">
+	
+
+                  <div class="panel panel-primary eqcols" style="border-radius:0px;">
+                     <div id="player-section" class="panel-body" style="border-radius:0px;">
+                        <div id="play"> 
+ <iframe allowfullscreen id="ipopp" src="/emb/61n-a.php" name="frame" scrolling="no" frameborder="no" align="center" height = "530px" width = "100%">
+                           </iframe>
+
+                        </div>
+                     </div>
+<button type="button" class="theater-button hidden-xs" style="position:relative; float:right; margin: 10px 0px 15px 10px" onclick="onTheaterMode()">Enter Theater Mode</button>
+
+
+                  </div>
+               </div>
+
+            </div>
+
+
+
+
+
+               <br />
+
+     <div class="row schedule2">
+               <div class="col-lg-9 col-lg-offset-1">
+<div class="panel panel-primary" style="border-radius:0px;">
+  <div class="panel-heading" style="border-radius:0px;min-height:20px !important;background:#00ACC1;border-color:#00ACC1;">
+    <h3 class="panel-title" style="font-family:'Titillium Web',sans-serif;font-weight:600;font-size:18px;"><i class="fa fa-twitch" aria-hidden="true"></i>&nbsp;Chat</h3>
+  </div>
+  <div class="panel-body" style="border-radius:0px;height:383px;overflow: hidden;">
+
+
+<div class="list-group" >
+</div> 
+ </div>
+</div>
+</div>
+</div>
+
+                  <br /> <br />
+               </div>
+            </div>
+
+            <br />
+            </div>
+         </div>
+      </div>
+<script>
+         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+         })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+         
+         ga('create', 'UA-103054419-1', 'auto');
+         ga('send', 'pageview');
+      </script>
+
+<script>
+$('.clsch').click(function(){
+$('.pood').toggle();
+});
+$('#opr').click(function(){
+$(this).hide();
+});
+$('#opz').click(function(){
+$(this).hide();
+});
+</script>
+   
+<!-- Go to www.addthis.com/dashboard to customize your tools --> <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-59a44f71ab56aed1"></script> 
+   </body>
+</html>
